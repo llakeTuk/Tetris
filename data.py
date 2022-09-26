4fb67ed0cad032f9924890b1afb23b00289544d7
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-09-27 01:28:48 +0300

    Update data.py

--- data.py
+++ data.py
@@ -228,5 +228,6 @@ class Board(QFrame):
 
         colorTable = [0x000000, 0xCC6666, 0x66CC66, 0x6666CC,
                       0xCCCC66, 0xCC66CC, 0x66CCCC, 0xDAAA00]
+
         color = QColor(colorTable[shape])
     
