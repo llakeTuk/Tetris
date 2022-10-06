3b1498c82145de680d9976ed2b4095a5755a1d47
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-10-07 00:02:32 +0300

    Update data.py

--- data.py
+++ data.py
@@ -241,4 +241,11 @@ class Board(QFrame):
 class Tetrominoe(object):
 
     NoShape = 0
+    ZShape = 1
+    SShape = 2
+    LineShape = 3
+    TShape = 4
+    SquareShape = 5
+    LShape = 6
+    MirroredLShape = 7
     
